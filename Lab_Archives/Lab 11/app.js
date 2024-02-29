07fbc310624b3e049022b2c8f24a4da601529f8d
Zathiald <A01705403.com>
2024-02-28 19:26:43 -0600

    Added new route and time reading

--- Lab_Archives/Lab 11/app.js
+++ Lab_Archives/Lab 11/app.js
@@ -16,18 +16,28 @@ const footer=
       </div>
     </footer>
   </body>
-<script>
-  function cambiarEstilo() {
-  document.getElementById("miParrafo").className = "is-size-1 has-text-danger";
-  document.getElementById("miParrafo").innerHTML = "BOO! ah te espante";
-}
+  <script>
 
-function restaurarEstilo() {
-  document.getElementById("miParrafo").className = "is-size-4";
-  document.getElementById("miParrafo").innerHTML = "Pasa el cursor sobre este texto";
-}
-</script>
-</html>`;
+  function cambiarEstilo() {
+    document.getElementById("miParrafo").className = "is-size-1 has-text-danger";
+    document.getElementById("miParrafo").innerHTML = "BOO! ah te espante";
+  };
+
+  function restaurarEstilo() {
+    document.getElementById("miParrafo").className = "is-size-4";
+    document.getElementById("miParrafo").innerHTML = "Pasa el cursor sobre este texto";
+
+    const fechaActual = new Date();
+    const fechaFormateada = fechaActual.toLocaleDateString();
+    const horaFormateada = fechaActual.toLocaleTimeString();
+  
+    document.getElementById('fecha').textContent = fechaFormateada;
+    document.getElementById('hora').textContent = horaFormateada;
+
+  };
+
+  </script>
+  </html>`;
 
 const header=
 `  <!DOCTYPE html>
@@ -102,8 +112,7 @@ const header=
         <section class="section">
             <div class="container">`;
 
-const construcciones = [{nombre: "casa", imagen: "https://i.blogs.es/7cfcd0/casas-en-minecraft/1366_2000.jpeg" , descripcion: "descripcion ejemplo"}];
-
+const construcciones = [{nombre: "casa", imagen: "https://i.blogs.es/7cfcd0/casas-en-minecraft/1366_2000.jpeg" , descripcion: "descripcion ejemplo", fecha_actual: new Date().toLocaleDateString(),hora_actual: new Date().toLocaleTimeString()}];
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -117,7 +126,7 @@ app.get('/sorpresa',(request,response,next)=>{
     console.log(request.body);
     response.send(header +`
     <div class="container">
-    <h1 class="title has-text-centered is-italic is-1 is-family-code">Lab 6</h1>
+    <h1 class="title has-text-centered is-italic is-1 is-family-code">WOOOW encontraste mi pagina secreta muajaajja</h1>
     <p id='result'></p>
 
     <p id="miParrafo" class="is-size-4" onmouseover="cambiarEstilo()" onmouseout="restaurarEstilo()">Texto sorpresa</p>
@@ -125,9 +134,34 @@ app.get('/sorpresa',(request,response,next)=>{
 
 });
 
+app.get('/info',(request,response,next)=>{
+  console.log(request.body);
+  response.send(header + 
+    `<blockquote> El archivo package.json es un componente fundamental en proyectos de JavaScript y es utilizado por npm, el gestor de paquetes más popular. Contiene metadatos importantes sobre el proyecto, como su nombre, versión, dependencias necesarias y scripts disponibles para su ejecución </blockquote>
+    <p class="has-text-centered"> Unos de los campos mas comunes en un archivo package.json son:</p> 
+    <ol type="I">
+    <li> name: Nombre del proyecto, librería o paquete. Se recomienda que coincida con el nombre del repositorio.</li>
+    <li> version:  Versión del paquete. Generalmente se utiliza semver (Semantic Versioning) para especificar versiones.</li>
+    <li> description: Breve descripción del paquete o proyecto. </li>
+    <li> main: Punto de entrada del proyecto. Por lo general, es index.js (para Node.js) o index.html (para navegadores).</li>
+    <li> scripts: Colección de scripts del proyecto. Estos scripts pueden ser ejecutados con comandos como npm run <script-name>.</li>
+    <li> keywords: Palabras clave relacionadas con el proyecto. Útil para búsquedas.</li> 
+    <li> author: Nombre del autor del paquete o proyecto.</li>
+    <li> license: Tipo de licencia del paquete o proyecto. Por defecto, suele ser ISC.</li>
+    <li> dependencies:  Colección de paquetes necesarios para producción y sus versiones instaladas.</li>
+    <li> devDependencies: Colección de paquetes necesarios para desarrollo y sus versiones instaladas.</li>
+    <li> homepage: URL de la página principal del paquete.</li>
+    <li> repository: URL del repositorio. Debe indicar el tipo (por ejemplo, git) y la ruta.</li>
+    <li> bugs: Objeto con un campo url que apunta a la página de issues del proyecto.</li>
+    </ol>` + footer);
+
+});
+
 app.get('/construir',(request,response, next) => {
     console.log(request.body);
-    response.send(header + `<h1 class="title">CONSTRUYE CONSTRUYE MAMAHUEVO</h1>
+    const fecha = new Date().toLocaleDateString();
+    const hora = new Date().toLocaleTimeString()
+    response.send(header + `<h1 class="title">CONSTRUYE TU REPERTORIO DE IMAGENES</h1>
     <form action="construir" method="POST">
         <label class="label" for="nombre">Nombre</label>
         <input name="nombre" id="nombre" type="text" class="input"><br>
@@ -135,7 +169,9 @@ app.get('/construir',(request,response, next) => {
         <input name="imagen" id="imagen" type="text" class="input"><br><br>
         <label class="label" for="descripcion">Descripcion</label>
         <input name="descripcion" id="descripcion" type="text" class="input"><br>
-        <input class="button-is-danger" type="submit" value="Construir">
+        <input type="hidden" name= "fecha_actual" id="fecha_actual"value=${fecha}>
+        <input type="hidden" name= "hora_actual" id="hora_actual"value=${hora}>
+        <input class="button is-danger" type="submit" value="Construir">
     </form>` + footer);
 });
 
@@ -175,7 +211,8 @@ app.get('/',(request, response, next) => {
                   <a>@bulmaio</a>.
                   <a href="#">#css</a> <a href="#">#responsive</a>
                   <br>
-                  <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  <time>${construccion.fecha_actual}</time>
+                  <time>${construccion.hora_actual}</time>
                 </div>
               </div>
             </div>
@@ -273,7 +310,6 @@ app.use((request,response,next)=>{
     <section class="section">
     <div class="container">
         <h1 class="title">Ups,no existe tu mundo!</h1>
-        <h2 class="is-bold">Pobre triste pendejo</h2>
         <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ae2dd6cb-d761-4361-a4fb-c278ed98e7e0/detqtlt-d5792735-d39e-4081-ba59-90e1021d52b2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FlMmRkNmNiLWQ3NjEtNDM2MS1hNGZiLWMyNzhlZDk4ZTdlMFwvZGV0cXRsdC1kNTc5MjczNS1kMzllLTQwODEtYmE1OS05MGUxMDIxZDUyYjIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MjhNrGbPzJZzeqgMI4lRdS5E8n2X-Qe_AOWhE09gaec" alt="Placeholder image">
     </div>
 
