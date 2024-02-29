751cd3b0eceffd51c73c6720b4e63f506a37f04f
Zathiald <A01705403.com>
2024-02-29 10:18:08 -0600

    Modified folders

--- Lab_Archives/Lab 11/app.js
+++ Lab_Archives/Lab 11/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','views');
+
 const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const bodyParser = require('body-parser');
 
