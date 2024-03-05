16ed0f899d8285a41a57a4653079e1ae48dac85f
Zathiald <A01705403.com>
2024-03-05 10:19:36 -0600

    Added cookies

--- Lab_Archives/Lab 11/controllers/construcciones.controller.js
+++ Lab_Archives/Lab 11/controllers/construcciones.controller.js
@@ -13,12 +13,24 @@ exports.post_construir = (request,response, next) => {
             request.body.fecha_actual,
             request.body.hora_actual)
     construccion.save();
+    response.setHeader('Set-Cookie','ultima_construccion=' +
+        request.body.nombre + '; HttpOnly');
     response.redirect('/');
 };
 
 exports.get_root = (request, response, next) => {
+    console.log('Ruta /');
+    let cookies = request.get('Cookie');
+    if (cookies){
+        cookies = cookies.split('=')[1];
+    }
+    else{
+        cookies = '';
+    }
+    console.log(cookies);
     response.render('raiz',{
      construcciones: Construccion.fetchAll(),
+     cookies: cookies,
     });
  };
 
--- Lab_Archives/Lab 11/models/emulador.model.js
+++ Lab_Archives/Lab 11/models/emulador.model.js
@@ -1,12 +1,22 @@
-const emuladores = [{
+const emuladores = [
+    {
     nombre: "Super Mario Bros.", 
+    descripcion: "1985",
     imagen: "https://i.etsystatic.com/6824854/r/il/53debe/585887395/il_1080xN.585887395_gpan.jpg" , 
-    enlace: "https://games.construct.net/769/latest", 
-}];
+    enlace: "https://games.construct.net/769/latest",
+    },
+    {
+    nombre: "The Legend of Zelda", 
+    descripcion: "1986",
+    imagen: "https://i.ebayimg.com/thumbs/images/g/nPIAAOSwFedktqbB/s-l640.jpg" , 
+    enlace: "https://www.webmulator.com/games/nintendo/super-mario?emulator_id=1",
+    }
+];
 
 module.exports = class Emulador {
-    constructor(mi_nombre, mi_imagen, mi_enlace) {
+    constructor(mi_nombre, mi_descripcion,mi_imagen, mi_enlace) {
         this.nombre = mi_nombre;
+        this.descripcion = mi_descripcion;
         this.imagen = mi_imagen;
         this.enlace = mi_enlace;
     }
@@ -15,6 +25,7 @@ module.exports = class Emulador {
     save() {
         emuladores.push({
             nombre: this.nombre,
+            descripcion: this.descripcion,
             imagen: this.imagen,
             enlace: this.enlace,
         });
