110d98b2a2c3e3d33459f6e6c7ec4cf0dbb1c57f
Zathiald <A01705403.com>
2024-03-05 13:35:17 -0600

    Added new emulators

--- Lab_Archives/Lab 11/models/emulador.model.js
+++ Lab_Archives/Lab 11/models/emulador.model.js
@@ -1,19 +1,33 @@
-
 const emuladores = [
     {
     nombre: "Super Mario Bros.", 
     descripcion: "1985",
     imagen: "https://i.etsystatic.com/6824854/r/il/53debe/585887395/il_1080xN.585887395_gpan.jpg" , 
-    enlace: "../public/roms/Mario.nes",
+    enlace: "https://games.construct.net/769/latest",
     },
+
     {
     nombre: "The Legend of Zelda", 
     descripcion: "1986",
     imagen: "https://i.ebayimg.com/thumbs/images/g/nPIAAOSwFedktqbB/s-l640.jpg" , 
-    enlace: "../public/roms/LegZelda.nes",
-    }
-];
+    enlace: "https://www.webmulator.com/games/nintendo/super-mario?emulator_id=1",
+    },
 
+    {
+    nombre: "Doom",
+    descripcion: "1993",
+    imagen: "https://store-images.s-microsoft.com/image/apps.22940.14303655336501012.2beb08d9-395e-453b-b5e3-0d4ac24d9d15.0a18faa9-5741-4971-99ce-55b5f0925b78",
+    enlace: "https://games.construct.net/53554/latest",
+    },
+
+    {
+        nombre: "Sans Fight",
+        descripcion: "2015",
+        imagen: "https://steamuserimages-a.akamaihd.net/ugc/1002520175786308448/13369F689EAAEBF62ED6E43D8210557D4C7C2B66/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+        enlace: "https://games.construct.net/60810/latest",
+    } 
+    
+];
 
 module.exports = class Emulador {
     constructor(mi_nombre, mi_descripcion,mi_imagen, mi_enlace) {
