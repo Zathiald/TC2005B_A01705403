925492c7686622e82dfcea60065b77395f6760f4
Zathiald <A01705403.com>
2024-03-07 11:22:37 -0600

    Reestrcutured models

--- Lab_Archives/Lab 11/models/construccion.model.js
+++ Lab_Archives/Lab 11/models/construccion.model.js
@@ -2,15 +2,16 @@ const db = require('../util/database');
 
 module.exports = class Construccion {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
-    constructor(mi_nombre,mi_imagen,mi_descripcion,mi_fecha_actual,mi_hora_actual) {
+    constructor(mi_nombre,mi_imagen,mi_descripcion,mi_fecha_actual,mi_hora) {
         this.nombre = mi_nombre;
         this.imagen = mi_imagen;
         this.descripcion = mi_descripcion;
+        this.hora = mi_hora;
     }
 
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-            return db.execute('INSERT INTO construccion (nombre, imagen, descripcion, username) VALUES (?, ?, ?, "samir27")',
+            return db.execute('INSERT INTO construccion (nombre, imagen, descripcion, username,created_at) VALUES (?, ?, ?, "samir27",?)',
             [this.nombre,this.imagen,this.descripcion]
         );
 
