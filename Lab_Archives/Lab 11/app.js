94442e774b9b9511b664d4552199eb112a3359c7
Zathiald <A01705403@tec.mx>
2024-04-01 09:51:54 -0600

    Agregar multer y funcion de subir archivos al servidor

--- Lab_Archives/Lab 11/app.js
+++ Lab_Archives/Lab 11/app.js
@@ -21,16 +21,33 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-//Proteccion contra ataques de CSRF
-const csrf = require('csurf');
-const csrfProtection = csrf();
-app.use(csrfProtection); 
 
 //Middleware
 app.use((request, response, next) => {
     console.log('Middleware!');
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
+
+const multer = require('multer')
+
+//fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'public/uploads');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null, file.originalname);
+    },
+});
+
+app.use(multer({ storage: fileStorage }).single('imagen')); 
+
+const csrf = require('csurf');
+const csrfProtection = csrf();
+app.use(csrfProtection); 
 
 const rutasApp = require('./routes/construcciones.routes')
 
